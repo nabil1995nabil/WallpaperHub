0b59a7711d4937616d701427c75aaf5790fc69fd
nabil1995nabil <73758493+nabil1995nabil@users.noreply.github.com>
2026-08-11 03:25:35 +0100

    Add files via upload

--- js/components.js
+++ js/components.js
@@ -94,3 +94,79 @@ document.body.appendChild(script);
 
 
 }
+
+// ==============================
+// Load Components
+// ==============================
+
+
+// AI Button
+
+const aiButton =
+document.getElementById("aiButton");
+
+
+if(aiButton){
+
+fetch("/components/ai-button.html")
+
+.then(response=>response.text())
+
+.then(html=>{
+
+aiButton.innerHTML = html;
+
+
+const script =
+document.createElement("script");
+
+
+script.src =
+"/js/ai-button.js";
+
+
+document.body.appendChild(script);
+
+
+});
+
+}
+
+
+
+
+
+// Notification Button
+
+const notificationButton =
+document.getElementById("notificationButton");
+
+
+if(notificationButton){
+
+fetch("/components/notification-button.html")
+
+.then(response=>response.text())
+
+.then(html=>{
+
+
+notificationButton.innerHTML = html;
+
+
+
+const script =
+document.createElement("script");
+
+
+script.src =
+"/js/notification-button.js";
+
+
+document.body.appendChild(script);
+
+
+
+});
+
+}
